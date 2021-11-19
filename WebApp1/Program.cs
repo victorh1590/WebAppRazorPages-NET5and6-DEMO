c5a0177fbc29a00a93d528833c224797c5db051a
Victor H F <51718734+victorh1590@users.noreply.github.com>
2021-11-19 14:25:48 -0300

    Downgrades .NET6 webapp Program.cs template to .NET 5 template. The default content of the .NET6 template is on the NET6Program file. Adds the Startup.cs class as well.

--- WebApp1/Program.cs
+++ WebApp1/Program.cs
@@ -1,25 +1,11 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
-// Add services to the container.
-builder.Services.AddRazorPages();
+IHostBuilder CreateHostBuilder(string[] args) =>
+    Host.CreateDefaultBuilder(args)
+        .ConfigureWebHostDefaults(webBuilder =>
+        {
+            webBuilder.UseStartup<Startup>();
+        });
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
-
-app.Run();
+CreateHostBuilder(args).Build().Run();
